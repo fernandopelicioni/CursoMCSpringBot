614254859dce7f4754c6402bbdb950ddcf2db1f9
Fernando Pelicioni <fernandopelicioni@hotmail.com>
2018-09-13 10:12:45 -0300

    Projeto Criado

---
+++ src/main/java/com/genoma/springbot1/SpringbotApplication.java
@@ -0,0 +1,12 @@
+package com.genoma.springbot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotApplication.class, args);
+	}
+}
